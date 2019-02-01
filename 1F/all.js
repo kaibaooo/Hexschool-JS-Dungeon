baae6ed007a4653c55835b09acb53bd01fd6a408
Kai-Lun Huang <kevin7776382@gmail.com>
2019-02-01 21:56:10 +0800

    dir change

---
+++ 1F/all.js
@@ -0,0 +1,33 @@
+let container = document.querySelector('.container');
+// let newBlock = ``;
+for (let i = 2; i <= 9; i++){
+    let section1 = ``;
+    let section2 = ``;
+    for (let j = 1; j <= 9; j++){
+        if (j <= 3) {
+            section1 += `<li>${i} x ${j} = ${i*j}</li>`
+        }
+        else {
+            section2 += `<li>${i} x ${j} = ${i*j}</li>`
+        }
+    }
+    // console.log(section1)
+    // console.log(section2)
+    console.log(container)
+    container.innerHTML += `
+    <div class="block">
+            <div class="inner-block">
+                <span class="number">${i}</span>
+                <ui class="formula">
+                    ${section1}
+                </ui>
+            </div>
+            <div class="inner-block-col2">
+                <ui class="formula">
+                    ${section2}
+                </ui>
+            </div>
+        </div>
+    `
+    
+}
