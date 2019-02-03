52ef8dc9f543978624b3a893dd20b8467cbf2b30
Kai-Lun Huang <kevin7776382@gmail.com>
2019-02-04 00:03:12 +0800

    init jump hand fix

--- 2F/asserts/js/all.js
+++ 2F/asserts/js/all.js
@@ -3,8 +3,10 @@ var sec = document.querySelector('.sec')
 var min = document.querySelector('.min')
 var hour = document.querySelector('.hour')
 // console.log(sec)
+cycle()
+setInterval(cycle, 1000)
 
-setInterval(()=>{
+function cycle(){
     let date = new Date;
     let currentSec= date.getSeconds();
     let currentMin= date.getMinutes();
@@ -13,4 +15,4 @@ setInterval(()=>{
     sec.style.transform = `rotate(${currentSec * 6-180}deg)`
     min.style.transform = `rotate(${currentMin* 6 + currentSec / 10}deg)`
     hour.style.transform = `rotate(${currentHour%12 * 30 - 90  + currentMin / 60 * 30}deg)`
-}, 1000)
+}
