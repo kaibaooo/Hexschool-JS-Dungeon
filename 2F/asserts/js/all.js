09f388e5e30f5f31acea243136e5551ef337dbfc
Kai-Lun Huang <kevin7776382@gmail.com>
2019-02-04 00:00:59 +0800

    2F semiFinal

---
+++ 2F/asserts/js/all.js
@@ -0,0 +1,16 @@
+
+var sec = document.querySelector('.sec')
+var min = document.querySelector('.min')
+var hour = document.querySelector('.hour')
+// console.log(sec)
+
+setInterval(()=>{
+    let date = new Date;
+    let currentSec= date.getSeconds();
+    let currentMin= date.getMinutes();
+    let currentHour= date.getHours();
+    // console.log(currentSec * 6, currentMin* 6, currentHour%12 * 30)
+    sec.style.transform = `rotate(${currentSec * 6-180}deg)`
+    min.style.transform = `rotate(${currentMin* 6 + currentSec / 10}deg)`
+    hour.style.transform = `rotate(${currentHour%12 * 30 - 90  + currentMin / 60 * 30}deg)`
+}, 1000)
