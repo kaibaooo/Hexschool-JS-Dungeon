3504756f12f79483b6853eb30b751f324970e992
Kai-Lun Huang <kevin7776382@gmail.com>
2019-03-13 00:44:40 +0800

    4F finished?

--- 4F/asserts/js/all.js
+++ 4F/asserts/js/all.js
@@ -1,28 +1,74 @@
 let timeOffset = [-12,-8,-1,0,3];
 let loc = ['NEW YORK', 'LONDON', 'BANGKOK', 'TAIWAN', 'SYDNEY'];
-let month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+let monthCode = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+let monthDay = [31,28,31,30,31,30,31,31,30,31,30,31];
 let contentContainer = document.querySelector('.content');
-for (let i = 0; i < timeOffset.length; i++){
-    let date = new Date();
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let day;
-    if (hour + timeOffset[i] < 0) {
-        day = `${date.getDate()} ${month[date.getMonth()]}. ${date.getFullYear()}`;    
+function updateClock(){
+    contentContainer.innerHTML = '';
+    for (let i = 0; i < timeOffset.length; i++){
+        let date = new Date();
+        let hour = date.getHours();
+        let day = date.getDate();
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let min = date.getMinutes();
+        hour+=timeOffset[i];
+        //閏年處理
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            monthDay[1] = 29;
+        else
+            monthDay[1] = 28;
+        //時間判斷
+        if(hour > 23){
+            //超過23時 算隔天
+            hour -= 23;
+            day += 1;
+            if(day > monthDay[month]){
+                //若該日超過當月最大天數，月份加加
+                month += 1;
+                day = 1;
+                if(month > 11){
+                    //若超過12個月，年分加加 && 月份歸為1月
+                    month = 0;
+                    year += 1;
+                }
+            }
+        }
+        else if(hour < 0){
+            //少於0時，算前一天
+            hour = 24 + hour;
+            day -= 1;
+            if(day < 0){
+                //若該日低於0號，月份減減
+                month -= 1;
+                if(month < 0){
+                    month = 11;
+                    year -= 1;
+                    //若少於1月，回到去年
+                }
+                day = monthDay[month];
+            }
+        }
+        dayInfo = `${date.getDate()} ${monthCode[date.getMonth()]}. ${date.getFullYear()}`;    
+        let dayAndNight = 0; // 0 day 1 night
+        if(hour < 5 || hour > 18) dayAndNight = 1;
+        else dayAndNight = 0;
+        contentContainer.innerHTML += `
+        <div class="element ${dayAndNight?'night':'day'}">
+            <div class="loc-info">
+                <div class="loc">${loc[i]}</div>
+                <div class="date">${dayInfo}</div>
+            </div>
+            <div class="clock">
+                ${hour}:${min}
+            </div>
+        </div>
+        `
     }
-    contentContainer.innerHTML += `
-    <div class="element night">
-        <div class="loc-info">
-            <div class="loc">${loc[i]}</div>
-            <div class="date">${day}</div>
-        </div>
-        <div class="clock">
-            ${hour+timeOffset[i]}:${min}
-        </div>
-    </div>
-    `
 }
 // 前一小時是前一天 if hour < 0 || hour > 12
 // 前一天是上個月 if 
 // 前一天是去年
 // 
+updateClock();
+setInterval(updateClock,1000);
