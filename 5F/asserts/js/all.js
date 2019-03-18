bf28a1352571768ac41ad9a92c4465978e8f399a
Kai-Lun Huang <kevin7776382@gmail.com>
2019-03-19 00:23:46 +0800

    trying api

--- 5F/asserts/js/all.js
+++ 5F/asserts/js/all.js
@@ -1,1 +1,19 @@
 //http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json
+const app = new Vue({
+    el:'.wrapper',
+    data:{
+
+    },
+    mounted: function(){
+        fetch('http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json',{
+            mode: 'cors',
+        })
+        .then(function(res){
+            console.log(res);
+            return res.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+    }
+})
