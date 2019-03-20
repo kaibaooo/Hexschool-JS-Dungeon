9311f204ce0e87976b4f788aabc3187fb211f126
Kai-Lun Huang <kevin7776382@gmail.com>
2019-03-20 18:30:54 +0800

    almost function, select  site to be finished

--- 5F/asserts/js/all.js
+++ 5F/asserts/js/all.js
@@ -1,19 +1,134 @@
-//http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json
+
 const app = new Vue({
-    el:'.wrapper',
+    el:'#wrapper',
     data:{
+        url : "http://34.80.220.180:3000/corkiller/http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json",
+        AQIData : '',
+        AQINewData : {},
+        currentCity : '屏東縣',
+        currentSite : '',
+        AQIColor:'good',
+        AQI : '',
+        O3 : '',
+        PM10 : '',
+        PM2dot5 : '',
+        CO : '',
+        SO2 : '',
+        NO2 : '',
+        PublishTime : '',
+        selectedSite : [],
 
     },
-    mounted: function(){
-        fetch('http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json',{
-            mode: 'cors',
-        })
-        .then(function(res){
-            console.log(res);
-            return res.json();
-        })
-        .then(function(data){
-            console.log(data);
-        })
+    mounted:function(){
+        fetch(this.url)
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                this.AQIData = json;
+                this.beautifyData();
+                this.createOptions();
+                this.changeCity();
+                console.log(this.AQINewData)
+            });
+    },
+    methods:{
+        beautifyData: function(){
+            for(let ele of this.AQIData){
+                if(this.AQINewData[ele['County']] == undefined){
+                    this.AQINewData[ele['County']] = []
+                    this.AQINewData[ele['County']].push({
+                        'name': ele['SiteName'],
+                        'AQI':  ele['AQI'],
+                        'O3':  ele['O3'],
+                        'PM10':  ele['PM10'],
+                        'PM2.5':  ele['O3'],
+                        'CO':  ele['CO'],
+                        'SO2':  ele['SO2'],
+                        'NO2':  ele['NO2'],
+                        'PublishTime' : ele['PublishTime']
+                    });
+                }
+                else{
+                    this.AQINewData[ele['County']].push({
+                        'name': ele['SiteName'],
+                        'AQI':  ele['AQI'],
+                        'O3':  ele['O3'],
+                        'PM10':  ele['PM10'],
+                        'PM2.5':  ele['O3'],
+                        'CO':  ele['CO'],
+                        'SO2':  ele['SO2'],
+                        'NO2':  ele['NO2'],
+                        'PublishTime' : ele['PublishTime']
+                    })
+                }
+            }
+        },
+        createOptions:function(){
+            for(let ele in this.AQINewData){
+                console.log(ele);
+                document.querySelector('#location').innerHTML += `<option value="${ele}">${ele}</option>`;
+            }
+        },
+        changeCity:function(){
+            let selectedCity = this.AQINewData[this.currentCity];
+            this.currentSite = selectedCity[0].name;
+            this.AQI = selectedCity[0].AQI;
+            this.O3 = selectedCity[0].O3;
+            this.PM10 = selectedCity[0].PM10;
+            this.PM2dot5 = selectedCity[0]['PM2.5'];
+            this.CO = selectedCity[0].CO;
+            this.SO2 = selectedCity[0].SO2;
+            this.NO2 = selectedCity[0].NO2; 
+            this.PublishTime = selectedCity[0].PublishTime;
+            document.querySelector('.general-info').innerHTML = `
+            <h3 class="current-area">${this.currentSite}</h3>
+                <h3 class="current-AQI ${this.fillColor(this.AQI)}">${this.AQI}</h3>
+                <div class="clear"></div>
+            `
+            document.querySelector('.areas').innerHTML = '';
+            for(let idx in selectedCity){
+                let addColorCity = selectedCity[idx];
+                console.log(this.fillColor(selectedCity[idx].AQI));
+                addColorCity['color'] = this.fillColor(selectedCity[idx].AQI);
+                this.selectedSite.push(selectedCity[idx]);
+                // document.querySelector('.areas').innerHTML+=`
+                // <div class="area-info">
+                //     <h3 class="name ${this.fillColor(selectedCity[idx].AQI)}">${selectedCity[idx].name}</h3>
+                //     <h3 class="AQI">${selectedCity[idx].AQI}</h3>
+                //     <div class="clear"></div>
+                // </div>   
+                // `
+            }
+            console.log(this.selectedSite);
+            let areaNumber = document.querySelectorAll('.area-info');
+            
+        },
+        changeSite:function(e, name){
+            console.log(e.currentTarget.children[0].textContent);
+            console.log(name);
+        },
+        fillColor:function(val){
+            if(val <= 50){
+                return 'good';
+            }
+            else if(val <= 100){
+                return 'normal';
+            }
+            else if(val <= 150){
+                return 'sensitive';
+            }
+            else if(val <= 200){
+                return 'unhealth';
+            }
+            else if(val <= 300){
+                return 'badhealth';
+            }
+            else{
+                return 'danger';
+            }
+        }
+
     }
+
 })
